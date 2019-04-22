5cf9420d19cf715678aabe2d29cb65da9bd91f37
Brendan Howell <bhowell841@gmail.com>
2019-04-21 22:37:44 -0400

    disaster masterpiece

--- public/app.js
+++ public/app.js
@@ -1,177 +1,66 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-//     $('.modal').modal();
-
-    // $(document).on('click', '#clear-articles', function() {
-    //     $.ajax({
-    //         url: "/articles",
-    //         type: "DELETE"
-    //     }).then(function(res) {
-    //         location.reload();
-    //     })
-    // });
-
-//     $(document).on("click", "#scrape-articles", function(){
-//         $.ajax({
-//             url: "/scrape",
-//             type: "GET"
-//         }).then(function(res) {
-//             location.reload();
-//         })
-//     });
-
-//     $(document).on("click", "#save-btn", function(){
-//         let id = $(this).attr("id");
-//         console.log(id);
-//         $.ajax({
-//             url: "/saved/" + id,
-//             type: "PUT",
-//             data: id,
-//         }).then(function(res){
-//             if(res){
-//                 $("#" + id).remove();
-//             }
-//         })
-//     })
-
-    
-//     $(document).on("click", ".delete-btn", function(){
-//         let id = $(this).attr("id");
-//         console.log(id);
-//         $.ajax({
-//             url: "/removed/" + id,
-//             type: "PUT",
-//             data: id,
-//         }).then(function(res) {
-//             if(res){
-//                 $("#" + id).remove();
-//             }
-//         })
-//     });
-
-    
-//     $(document).on("click", ".note-btn", function(){
-//         let id = $(this).attr("id");
-//         $("#save-note").attr("data-id", id);
-//         $ajax({
-//             url: "/articles/" + id,
-//             type: "GET"
-//         }).then(function(re) {
-//             console.log(res);
-//             $("#note-input").text(res.note.body);
-//             M.updateTextFiled();
-//         })
-//     })
-
-//     $(document).on("click", "#save-note", function(){
-//         let id = $(this).attr("id");
-//         $.ajax({
-//             url: "/articles/" + id,
-//             type: "POST",
-//             data: {
-//                 body: $("#note-input")
-//             }
-//             }).then(function(res){
-//                 console.log(res) 
-//         })
-//     })
-
-//     let modal = document.querySelector(".modal");
-//     modal.init(modal,{
-//         onCloseEnd: function(){
-//             $("#note-input");
-//         }
-//     })
-// })
-
-$(document).ready(function () {
-
-    $('.modal').modal();
-
-    $(document).on('click', '#clear-articles', function(){
+// Good but needs refining
+    $(document).on('click', '#clear-articles', function() {
         $.ajax({
             url: "/articles",
             type: "DELETE"
-        }).then(function(res){
+        }).then(function(res) {
             location.reload();
+        })
+    });
+// Good
+    $(document).on("click", "#scrape-articles", function(){
+        $.ajax({
+            url: "/scrape",
+            type: "GET"
+        }).then(function(res) {
+            location.reload();
+        })
+    });
+// not working
+    $(document).on("click", ".delete-btn", function() {
+        event.preventDefault();
+        const id = $(this).attr("data");
+        console.log(id)
+        $.ajax({
+            method: "PUT",
+            url: "/delete/" + id
+        }).done(function() {
+            location.reload();
+        })
+    });
+
+// definately not working 
+    $(document).on("click", ".note-btn", function(){
+        let id = $(this).attr("data");
+        console.log("I hit the note button")
+        $("#save-note").attr("data-id", id);
+        $ajax({
+            url: "/articles/" + id,
+            type: "GET"
+        }).then(function(re) {
+            console.log(res);
+            $("#note-input").text(res.note.body);
+            M.updateTextFiled();
         })
     })
 
-
-
-
-    // event handler for deleting a note
-    $(".delete-btn").click(function (event) {
-        event.preventDefault();
-        const id = $(this).attr("data");
-        $.ajax(`/remove/${id}`, {
-            type: "PUT"
-        }).then(function(){
-            location.reload();
-        })
-    });
-    
-    // event handler for opening the note modal
-    $(".note-btn").click(function (event) {
-        event.preventDefault();
-        const id = $(this).attr("data");
-        $('#article-id').text(id);
-        $('#save-note').attr('data', id);
-        $.ajax(`/articles/${id}`, {
-            type: "GET"
-        }).then(function (data) {
-            console.log(data)
-            $('.articles-available').empty();
-            if (data[0].note.length > 0){
-                data[0].note.forEach(v => {
-                    $('.articles-available').append($(`<li class='list-group-item'>${v.text}<button type='button' class='btn btn-danger btn-sm float-right btn-deletenote' data='${v._id}'>X</button></li>`));
-                })
-            }
-            else {
-                $('.articles-available').append($(`<li class='list-group-item'>No notes for this article yet</li>`));
-                console.log("Second ran!")
-            }
-        })
-        $('#note-modal').modal('toggle');
-    });
-
-    // $('.btn-deletenote').click(function (event) {})
-    $(document).on('click', '.btn-deletenote', function (){
-            event.preventDefault();
-            console.log($(this).attr("data"))
-            const id = $(this).attr("data");
-            console.log(id);
-            $.ajax(`/note/${id}`, {
-                type: "DELETE"
-            }).then(function () {
-                $('#note-modal').modal('toggle');
-            });
-    });
-
-    $("#save-note").click(function (event) {
-        event.preventDefault();
-        const id = $(this).attr('data');
-        const noteText = $('#note-input').val().trim();
-        $('#note-input').val('');
-        $.ajax(`/note/${id}`, {
+// I can't get the modal to even fucking pop
+    $(document).on("click", "#save-note", function(){
+        let id = $(this).attr("id");
+        $.ajax({
+            url: "/articles/" + id,
             type: "POST",
-            data: { text: noteText}
-        }).then(function (data) {
-            console.log(data)
+            data: {
+                body: $("#note-input")
+            }
+            }).then(function(res){
+                console.log(res) 
         })
-        $('#note-modal').modal('toggle');
-    });
+    })
 
-    // $('#clear-articles').click(function(event) {
-    //     $.ajax({
-    //         url: "/articles",
-    //         type: "DELETE"
-    //     }).then(function(res) {
-    //         location.reload();
-    //     })
-    // });
-
-    $(".save-btn").click(function(event) {
+    $(document).on("click", ".save-btn", function() {
         event.preventDefault();
         const button = $(this);
         const id = button.attr("id");
@@ -179,7 +68,44 @@ $(document).ready(function () {
             type: "PUT"
         }).then(function() {
             button.text("Article Saved");
-            }
-        );
-    });
+        })
+    })
+   
+   
+
+
+
+
+    
+
+
+    // // $('.btn-deletenote').click(function (event) {})
+    // $(document).on('click', '.btn-deletenote', function (){
+    //         event.preventDefault();
+    //         console.log($(this).attr("data"))
+    //         const id = $(this).attr("data");
+    //         console.log(id);
+    //         $.ajax(`/note/${id}`, {
+    //             type: "DELETE"
+    //         }).then(function () {
+    //             $('#note-modal').modal('toggle');
+    //         });
+    // });
+
+//     $("#save-note").click(function (event) {
+//         event.preventDefault();
+//         const id = $(this).attr('data');
+//         const noteText = $('#note-input').val().trim();
+//         $('#note-input').val('');
+//         $.ajax(`/note/${id}`, {
+//             type: "POST",
+//             data: { text: noteText}
+//         }).then(function (data) {
+//             console.log(data)
+//         })
+//         $('#note-modal').modal('toggle');
+//     });
+
+
+    
 })
